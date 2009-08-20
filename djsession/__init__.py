fe3718adabfa4167a6d01429e111ba0df6b0749d
Ask Solem <askh@opera.com>
2009-08-20 14:12:41 +0200

    Initial import

---
+++ djsession/__init__.py
@@ -0,0 +1,7 @@
+"""Django Cache+DB Session wich uses table rotation for expiry"""
+VERSION = (0, 0, 1)
+__version__ = ".".join(map(str, VERSION))
+__author__ = "Ask Solem"
+__contact__ = "askh@opera.com"
+__homepage__ = "http://github.com/ask/django-sessionstore/"
+__docformat__ = "restructuredtext"
---
+++ docs/index.rst
@@ -0,0 +1,19 @@
+django-sessionstore Documentation
+==================================
+
+Contents:
+
+.. toctree::
+    :maxdepth: 3
+
+    introduction
+    changelog
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
