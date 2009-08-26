af108b2724af8ab85b5ee12a9fb2746ce1ccd58c
Batiste <batiste@batiste-laptop.(none)>
2009-08-26 14:42:54 +0200

    Add documentations and a few tests

--- djsession/backends/db.py
+++ djsession/backends/db.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from djsession.models import CurrentSession, PrevSession
 from django.contrib.sessions.backends.base import CreateError, SessionBase
 from django.contrib.sessions.backends.db import SessionStore as DBStore
@@ -6,44 +6,54 @@ from django.contrib.sessions.backends.db import SessionStore as DBStore
 from django.core.exceptions import SuspiciousOperation
 from django.db import IntegrityError, transaction
 from django.utils.encoding import force_unicode
+from django.conf import settings
 
 
 class SessionStore(DBStore):
-    """Get/save sessions from the database."""
+    """
+    This session store is able to work on 2 different session
+    tables.
+
+    One is the current session table, the other is an old session table.
+    If the session is not found in the current table, the class try on
+    the old table. Then the row is created in the current table if
+    nothing is found.
+    """
 
     current = CurrentSession
     previous = PrevSession
 
-    def __init__(self, session_key=None, current=None, previous=None,
-                                                    create_if_missing=True):
+    def __init__(self, session_key=None, current=None, previous=None):
         self.current = current or self.current
         self.previous = previous or self.previous
-        self.DoesNotExist = self.current.DoesNotExist
-        self.create_if_missing = create_if_missing
         super(SessionStore, self).__init__(session_key=session_key)
 
-    def _get_db_session(self):
+    def _get_db_session(self, session_key=None):
+        if session_key is None:
+            session_key=self.session_key
         try:
-            s = self.current.objects.get(session_key=self.session_key)
+            s = self.current.objects.get(session_key=session_key)
             return s
-        except (CurrentSession.DoesNotExist, SuspiciousOperation):
+        except (self.current.DoesNotExist, SuspiciousOperation):
             pass
         try:
-            s = self.previous.objects.get(session_key=self.session_key)
+            s = self.previous.objects.get(session_key=session_key)
+            # migrate to the current table
+            self._save(self.decode(force_unicode(s.session_data)))
             return s
-        except (PrevSession.DoesNotExist, SuspiciousOperation):
+        except (self.previous.DoesNotExist, SuspiciousOperation):
             return None
 
     def load(self):
         session = self._get_db_session()
         if session is None:
-            if self.create_if_missing:
-                self.create()
+            self.create()
             return {}
         return self.decode(force_unicode(session.session_data))
 
     def exists(self, session_key):
-        if self._get_db_session() is None:
+        session = self._get_db_session(session_key)
+        if session is None:
             return False
         return True
 
@@ -51,15 +61,13 @@ class SessionStore(DBStore):
         session_data = self._get_session(no_load=must_create)
         self._save(session_data, must_create=must_create)
 
-    def init_from_other(self, other, must_create=False):
-        session_data = other._get_session(no_load=False)
-        return self._save(session_data, must_create=must_create)
-
     def _save(self, session_data, must_create=False):
         session_data = self.encode(session_data)
+        expire_date = (datetime.utcnow() +
+                timedelta(seconds=settings.SESSION_COOKIE_AGE))
         obj = self.current(session_key=self.session_key,
                          session_data=session_data,
-                         expire_date=self.get_expiry_date())
+                         expire_date=expire_date)
         sid = transaction.savepoint()
         try:
             obj.save(force_insert=must_create)
@@ -73,3 +81,6 @@ class SessionStore(DBStore):
         session = self._get_db_session()
         if session is not None:
             session.delete()
+            session = self._get_db_session()
+            if session is not None:
+                session.delete()
